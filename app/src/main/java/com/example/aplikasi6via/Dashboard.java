2faf74a39c5e50a890deb9a99063a44c0b8f41e0
octaviabay <octaviabayuk@gmail.com>
2022-06-13 16:34:35 +0700

    Tugas

---
+++ app/src/main/java/com/example/aplikasi6via/Dashboard.java
@@ -0,0 +1,36 @@
+package com.example.aplikasi6via;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Dashboard extends AppCompatActivity {
+    String EmailHolder;
+    TextView Email;
+    Button LogOUT ;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+        Email = (TextView)findViewById(R.id.textEmail);
+        LogOUT = (Button)findViewById(R.id.buttonLogOut);
+        Intent intent = getIntent();
+        // Receiving User Email Send By MainActivity.
+        EmailHolder = intent.getStringExtra(MainActivity.UserEmail);
+        // Setting up received email to TextView.
+        Email.setText(Email.getText().toString()+ EmailHolder);
+        // Adding click listener to Log Out button.
+        LogOUT.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Finishing current DashBoard activity on button click.
+                finish();
+            }
+        });
+    }
+}
